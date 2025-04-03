5118b7f65a1e9cde295eed9f65c1219b172ff476
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-03 21:52:11 +0900

    各入力フィールドの要素の取得処理を追加

---
+++ Phase3/Assignment/js/validation.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 入力フィールドの要素の取得
+  const nameInput = document.getElementById("name");
+  const nameErrorMessage = document.getElementById("name-error-message");
+  const emailInput = document.getElementById("email");
+  const emailErrorMessage = document.getElementById("email-error-message");
+  const passwordInput = document.getElementById("password");
+  const passwordErrorMessage = document.getElementById("password-error-message");
+});
