740727e1d17c1c3ab90ae05ce8e216f7d134981c
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-07 22:22:41 +0900

    バリデーションメッセージの要素を一括で取得し、各エラーメッセージ要素を絞りこみ取得するよう修正

--- Phase3/Assignment/js/validation.js
+++ Phase3/Assignment/js/validation.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   // 入力フィールドの要素の取得
   const nameInput = document.getElementById("name");
-  const nameErrorMessage = document.getElementById("name-error-message");
   const emailInput = document.getElementById("email");
-  const emailErrorMessage = document.getElementById("email-error-message");
   const passwordInput = document.getElementById("password");
-  const passwordErrorMessage = document.getElementById("password-error-message");
+
+  // 各エラーメッセージ要素を取得
+  const validationMessages = document.querySelectorAll(".validation-message");
+  const nameErrorMessage = Array.from(validationMessages).find((message) =>
+    message.classList.contains("name")
+  );
+  const emailErrorMessage = Array.from(validationMessages).find((message) =>
+    message.classList.contains("email")
+  );
+  const passwordErrorMessage = Array.from(validationMessages).find((message) =>
+    message.classList.contains("password")
+  );
 
   // ページロード時にリアルタイムでバリデーションを実行
   validateName();
