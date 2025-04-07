01f6520a5c43e87712cb267801e83ef1572ebdae
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-07 22:33:43 +0900

    バリデーションのメソッドをaddEventListenerの外で定義

--- Phase3/Assignment/js/validation.js
+++ Phase3/Assignment/js/validation.js
@@ -1,4 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
+// 名前のリアルタイムバリデーション
+function validateName(nameInput, nameErrorMessage) {
+  const name = nameInput.value.trim();
+  nameErrorMessage.classList.remove("message-valid", "message-invalid");
+  if (name === "") {
+    nameErrorMessage.classList.add("message-invalid");
+    nameErrorMessage.textContent = "お名前を入力してください";
+  } else {
+    nameErrorMessage.classList.add("message-valid");
+    nameErrorMessage.textContent = "OK";
+  }
+}
+
+// メールアドレスのリアルタイムバリデーション
+function validateEmail(emailInput, emailErrorMessage) {
+  const email = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  emailErrorMessage.classList.remove("message-valid", "message-invalid");
+  if (email === "") {
+    emailErrorMessage.classList.add("message-invalid");
+    emailErrorMessage.textContent = "メールアドレスを入力してください";
+  } else if (!emailRegex.test(email)) {
+    emailErrorMessage.classList.add("message-invalid");
+    emailErrorMessage.textContent = "有効なメールアドレスを入力してください";
+  } else {
+    emailErrorMessage.classList.add("message-valid");
+    emailErrorMessage.textContent = "OK";
+  }
+}
+
+// パースワードのリアルタイムバリデーション
+function validatePassword(passwordInput, passwordErrorMessage) {
+  const password = passwordInput.value.trim();
+  passwordErrorMessage.classList.remove("message-valid", "message-invalid");
+  if (password.length < 8) {
+    passwordErrorMessage.classList.add("message-invalid");
+    passwordErrorMessage.textContent =
+      "パスワードは8文字以上で入力してください";
+  } else {
+    passwordErrorMessage.classList.add("message-valid");
+    passwordErrorMessage.textContent = "OK";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   // 入力フィールドの要素の取得
   const nameInput = document.getElementById("name");
   const emailInput = document.getElementById("email");
@@ -17,56 +61,12 @@ document.addEventListener("DOMContentLoaded", function() {
   );
 
   // ページロード時にリアルタイムでバリデーションを実行
-  validateName();
-  validateEmail();
-  validatePassword();
+  validateName(nameInput, nameErrorMessage);
+  validateEmail(emailInput, emailErrorMessage);
+  validatePassword(passwordInput, passwordErrorMessage);
 
   // フォーム入力時にリアルタイムでバリデーションを実行
   nameInput.addEventListener("input", validateName);
   emailInput.addEventListener("input", validateEmail);
   passwordInput.addEventListener("input", validatePassword);
-
-  // 名前のリアルタイムバリデーション
-  function validateName() {
-    const name = nameInput.value.trim();
-    nameErrorMessage.classList.remove("message-valid", "message-invalid");
-    if (name === "") {
-      nameErrorMessage.classList.add("message-invalid");
-      nameErrorMessage.textContent = "お名前を入力してください";
-    } else {
-      nameErrorMessage.classList.add("message-valid");
-      nameErrorMessage.textContent = "OK";
-    }
-  }
-
-  // メールアドレスのリアルタイムバリデーション
-  function validateEmail() {
-    const email = emailInput.value.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    emailErrorMessage.classList.remove("message-valid", "message-invalid");
-    if (email === "") {
-      emailErrorMessage.classList.add("message-invalid");
-      emailErrorMessage.textContent = "メールアドレスを入力してください";
-    } else if (!emailRegex.test(email)) {
-      emailErrorMessage.classList.add("message-invalid");
-      emailErrorMessage.textContent = "有効なメールアドレスを入力してください";
-    } else {
-      emailErrorMessage.classList.add("message-valid");
-      emailErrorMessage.textContent = "OK";
-    }
-  }
-
-  // パースワードのリアルタイムバリデーション
-  function validatePassword() {
-    const password = passwordInput.value.trim();
-    passwordErrorMessage.classList.remove("message-valid", "message-invalid");
-    if (password.length < 8) {
-      passwordErrorMessage.classList.add("message-invalid");
-      passwordErrorMessage.textContent =
-        "パスワードは8文字以上で入力してください";
-    } else {
-      passwordErrorMessage.classList.add("message-valid");
-      passwordErrorMessage.textContent = "OK";
-    }
-  }
-})
+  });
