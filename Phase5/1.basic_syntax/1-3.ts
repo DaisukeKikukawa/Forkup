3a0e5f09a69c96c31a571704ce994542939ffcd5
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-24 19:53:51 +0900

    1-3：配列を使用した数値の合計計算

---
+++ Phase5/1.basic_syntax/1-3.ts
@@ -0,0 +1,9 @@
+// 3.配列を使用した数値の合計計算
+const numArray: number[] = [10, 20, 30, 40, 50];
+let totalNum: number = 0;
+
+for (let i = 0; i < numArray.length; i++) {
+  totalNum += numArray[i];
+}
+
+console.log(totalNum);
