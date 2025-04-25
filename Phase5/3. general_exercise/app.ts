b7133c4508429b4072d6176d62e550aa501ac4b7
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-25 18:29:26 +0900

    メニュー選択の繰り返し処理の修正

--- Phase5/3. general_exercise/app.ts
+++ Phase5/3. general_exercise/app.ts
@@ -1,26 +1,23 @@
-import inquirer from "inquirer";
+const readlineSync = require("readline-sync");
 
-const menu_selections: string[] = [
-  "1.点数入力",
-  "2.成績一覧表示",
-  "3.成績分析",
-  "4.終了",
+let menu_options: string[] = [
+  "1. 点数入力",
+  "2. 成績一覧表示",
+  "3. 成績分析",
+  "4. 終了",
 ];
 
-async function showMenu() {
-  while (true) {
-    const answers = await inquirer.prompt([
-      {
-        type: "list",
-        name: "menu_selection",
-        message: "メニューを選択してください",
-        choices: menu_selections,
-      },
-    ]);
-    if (answers.menu_selection === "4.終了") {
-      break;
-    }
-  }
+const showMenu = ():string => {
+  console.log("=== メインメニュー ===");
+  menu_options.forEach((menu_option) => {
+    console.log(menu_option);
+  });
+  return readlineSync.question("選択してください：");
 }
 
-showMenu();
+while(true) {
+  let selected_menu: string = showMenu();
+  if (Number(selected_menu) === 4) {
+    break;
+  }
+}
