0633a13dce81fc82245a6348c78fbb898fc0e490
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-27 16:10:12 +0900

    成績一覧表示機能の作成

--- Phase5/3. general_exercise/app.ts
+++ Phase5/3. general_exercise/app.ts
@@ -39,8 +39,22 @@ const validateStudentScore = (studentScore: number): boolean => {
   }
 };
 
+const convert_score_into_evaluation = (studentScore: number): string => {
+  if (studentScore >= 90 && studentScore <= 100) {
+    return "A";
+  } else if (studentScore >= 80 && studentScore <= 89) {
+    return "B";
+  } else if (studentScore >= 70 && studentScore <= 79) {
+    return "C";
+  } else if (studentScore >= 60 && studentScore <= 69) {
+    return "D";
+  } else {
+    return "F";
+  }
+};
+
 const inputStudentInformation = () => {
-  let student = { number: 0, score: 0 };
+  let student = { number: 0, score: 0, evaluation: "" };
   let studentNumber: number = 0;
   let studentScore: number = 0;
 
@@ -68,7 +82,22 @@ const inputStudentInformation = () => {
   }
   student.score = studentScore;
 
+  student.evaluation = convert_score_into_evaluation(studentScore);
+
   students.push(student);
+};
+
+const showStudentRecord = () => {
+  console.log("=== 成績一覧 ===");
+  console.log("生徒番号    点数    評価");
+
+  students.forEach((student) => {
+    console.log(
+      `${String(student.number).padEnd(14)}${String(student.score).padEnd(9)}${
+        student.evaluation
+      }`
+    );
+  });
 };
 
 const showMenu = (): string => {
@@ -86,6 +115,7 @@ while (true) {
   if (Number(selectedMenu) === 1) {
     inputStudentInformation();
   } else if (Number(selectedMenu) === 2) {
+    showStudentRecord();
   } else if (Number(selectedMenu) === 3) {
   } else if (Number(selectedMenu) === 4) {
     break;
