89029271548ed7ad805cdcb28b30d185aad7f2db
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-04-30 19:40:50 +0900

    生徒に関する処理をstudent.tsファイルに定義

--- Phase5/3. general_exercise/app.ts
+++ Phase5/3. general_exercise/app.ts
@@ -1,62 +1,16 @@
 const readlineSync = require("readline-sync");
-import { Student } from "./student";
+import { inputStudentInformation, showStudentRecord, Student,  } from "./student";
 import { showMenu } from "./menu";
-import {
-  validateStudentNumberLargerThanZero,
-  validateDuplicateStudentNumber,
-  validateStudentScore,
-} from "./validation";
 import { convertScoreIntoEvaluation, showAnalyzedScore } from "./analyzeScore";
 
 const students: Student[] = [];
 
-const inputStudentInformation = () => {
-  const student = new Student();
-
-  while (true) {
-    student.id = Number(readlineSync.question("生徒番号を入力してください："));
-    if (validateStudentNumberLargerThanZero(student.id)) {
-      console.log("生徒番号は1以上で入力してください");
-    } else if (validateDuplicateStudentNumber(student.id,students)) {
-      console.log("その生徒番号はすでに登録済みです");
-    } else {
-      break;
-    }
-  }
-
-  while (true) {
-    student.score = Number(readlineSync.question("成績を入力してください："));
-    if (validateStudentScore(student.score)) {
-      console.log("1〜100の数字を入力してください")
-    } else {
-      break;
-    }
-  }
-
-  student.evaluation = convertScoreIntoEvaluation(student.score);
-
-  students.push(student);
-};
-
-const showStudentRecord = () => {
-  console.log("=== 成績一覧 ===");
-  console.log("生徒番号    点数    評価");
-
-  students.forEach((student) => {
-    console.log(
-      `${String(student.id).padEnd(14)}${String(student.score).padEnd(9)}${
-        student.evaluation
-      }`
-    );
-  });
-};
-
 while (true) {
   let selectedMenu: string = showMenu();
   if (Number(selectedMenu) === 1) {
-    inputStudentInformation();
+    inputStudentInformation(students);
   } else if (Number(selectedMenu) === 2) {
-    showStudentRecord();
+    showStudentRecord(students);
   } else if (Number(selectedMenu) === 3) {
     showAnalyzedScore(students);
   } else if (Number(selectedMenu) === 4) {
--- Phase5/3. general_exercise/student.ts
+++ Phase5/3. general_exercise/student.ts
@@ -1,3 +1,10 @@
+const readlineSync = require("readline-sync");
+import { convertScoreIntoEvaluation } from "./analyzeScore";
+import {
+  validateStudentNumberLargerThanZero,
+  validateDuplicateStudentNumber,
+  validateStudentScore,
+} from "./validation";
 export class Student {
   id: number;
   score: number;
@@ -9,3 +16,44 @@ export class Student {
     this.evaluation = "";
   }
 }
+
+export const inputStudentInformation = (students: Student[]) => {
+  const student = new Student();
+
+  while (true) {
+    student.id = Number(readlineSync.question("生徒番号を入力してください："));
+    if (validateStudentNumberLargerThanZero(student.id)) {
+      console.log("生徒番号は1以上で入力してください");
+    } else if (validateDuplicateStudentNumber(student.id, students)) {
+      console.log("その生徒番号はすでに登録済みです");
+    } else {
+      break;
+    }
+  }
+
+  while (true) {
+    student.score = Number(readlineSync.question("成績を入力してください："));
+    if (validateStudentScore(student.score)) {
+      console.log("1〜100の数字を入力してください");
+    } else {
+      break;
+    }
+  }
+
+  student.evaluation = convertScoreIntoEvaluation(student.score);
+
+  students.push(student);
+};
+
+export const showStudentRecord = (students: Student[]) => {
+  console.log("=== 成績一覧 ===");
+  console.log("生徒番号    点数    評価");
+
+  students.forEach((student) => {
+    console.log(
+      `${String(student.id).padEnd(14)}${String(student.score).padEnd(9)}${
+        student.evaluation
+      }`
+    );
+  });
+};
