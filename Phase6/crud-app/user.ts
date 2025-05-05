724ac03061ac84d75981a0c71e74bf31f250884f
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-05 22:41:09 +0900

    ユーザー登録（Create）機能の実装

--- Phase6/crud-app/user.ts
+++ Phase6/crud-app/user.ts
@@ -56,3 +56,64 @@ export const showUsers = async (connection) => {
     if (answer.trim() === "") break;
   }
 };
+export const registerUser = async (connection) => {
+  console.log("=== ユーザー登録 ===");
+  let name: string;
+  let email: string;
+  let age: number;
+
+  while (true) {
+    name = readlineSync.question("名前を入力してください： ");
+    if (validateName(name)) {
+      break;
+    } else {
+      console.log("名前は空にできません。再入力してください。");
+    }
+  }
+
+  while (true) {
+    email = readlineSync.question("メールアドレスを入力してください： ");
+    if (validateEmail(email)) {
+      break;
+    } else {
+      console.log("有効なメールアドレスを入力してください。");
+    }
+  }
+
+  while (true) {
+    age = Number(readlineSync.question("年齢を入力してください： "));
+    if (validateAge(age)) {
+      break;
+    } else {
+      console.log("年齢は0〜120の間で入力してください。");
+    }
+  }
+
+  const user = new User(name, email, age);
+
+  console.log("");
+  console.log("以下の内容で登録しますか？");
+  console.log(`名前：${user.getName()}`);
+  console.log(`メールアドレス：${user.getEmail()}`);
+  console.log(`年齢：${user.getAge()}`);
+  console.log("");
+  while (true) {
+    const answer = readlineSync.question("登録しますか？（y/n）");
+    if (answer === "y") {
+      try {
+        const [result, fields] = await connection.query(
+          "INSERT INTO users(name,email,age) VALUES(?,?,?)",
+          [user.getName(), user.getEmail(), user.getAge()]
+        );
+        console.log("ユーザーが正常に登録されました。");
+        break;
+      } catch (error) {
+        console.error("[エラー] このメールアドレスは既に登録されています");
+        break;
+      }
+    } else if (answer === "n") {
+      console.log("登録をキャンセルしました。");
+      break;
+    }
+  }
+};
