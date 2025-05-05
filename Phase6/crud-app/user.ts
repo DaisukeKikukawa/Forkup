14df7525025cd8a0f32ac0e4c0b83840d3dc1691
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-05 22:16:17 +0900

    Userクラスの作成

---
+++ Phase6/crud-app/user.ts
@@ -0,0 +1,39 @@
+import readlineSync from "readline-sync";
+import { validateName, validateEmail, validateAge } from "./validation.js";
+
+export class User {
+  private name: string;
+  private email: string;
+  private age: number;
+
+  constructor(name: string, email: string, age: number) {
+    this.name = name;
+    this.email = email;
+    this.age = age;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  getAge(): number {
+    return this.age;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  setEmail(email: string): void {
+    this.email = email;
+  }
+
+  setAge(age: number): void {
+    this.age = age;
+  }
+}
+
