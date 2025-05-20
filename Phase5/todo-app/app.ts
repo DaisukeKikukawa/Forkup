306932f3b996ddf4bc798ee03c5bd86f2730f1a3
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-21 00:50:23 +0900

    更新処理の作成
    登録済みTODOの更新処理

--- Phase5/todo-app/app.ts
+++ Phase5/todo-app/app.ts
@@ -1,3 +1,13 @@
+import * as http from "http";
+import * as fs from "fs";
+import { parse } from "querystring";
+import { startConnection } from "./db";
+import ejs from "ejs";
+import * as path from "path";
+import { getAllTodos } from "./db-operation";
+import * as url from "url";
+import { validateTodoData } from "./validation";
+
 const server = http.createServer(async(req, res) => {
   if (req.url === "/") {
     const todos = await getAllTodos();
@@ -47,6 +57,64 @@ const server = http.createServer(async(req, res) => {
       }
     });
   } else if (req.url?.startsWith("/edit") && req.method === "GET") {
+    const parsedUrl = url.parse(req.url, true);
+    const id = parsedUrl.query.id;
+    const conn = await startConnection();
+    const [rows] = await conn.execute("SELECT * FROM todos WHERE id = ?", [id]);
+    await conn.end();
+
+    const todo = Array.isArray(rows) ? rows[0] : null;
+
+    const filePath = path.join(__dirname, "views", "edit.html.ejs");
+    const html = await ejs.renderFile(filePath, { todo, errors: [] });
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (req.url === "/update" && req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", async () => {
+      const parsed = parse(body);
+      const id = parsed["id"];
+      const title = parsed["title"] as string;
+      const description = parsed["description"] as string;
+
+      const errors = validateTodoData(title, description);
+
+      if (errors.length === 0) {
+        const conn = await startConnection();
+        await conn.execute(
+          "UPDATE todos SET title = ?, description = ? WHERE id = ?",
+          [title, description, id]
+        );
+        await conn.end();
+
+        res.writeHead(302, { Location: "/" });
+        res.end();
+      } else {
+        const conn = await startConnection();
+        const [rows] = await conn.execute("SELECT * FROM todos WHERE id = ?", [
+          id,
+        ]);
+        await conn.end();
+
+        const todo = {
+          id: id,
+          title: title,
+          description: description,
+        };
+
+        const filePath = path.join(__dirname, "views", "edit.html.ejs");
+        const html = await ejs.renderFile(filePath, { todo, errors });
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
+      }
+    });
 });
 
 server.listen(3000, () => {
